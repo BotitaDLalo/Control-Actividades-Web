e8bd58ddd19db9a9a300394d787008db3189df44
BotitaDLalo <canciondfuego0@gmail.com>
2026-01-01 22:16:25 -0600

    Método para  obtener Id del usuario
    Con este metodo obtener el id interno del usuario (docenteId o alumnoId) en base a su Id de identity

--- Recursos/FuncionalidadesGenerales.cs
+++ Recursos/FuncionalidadesGenerales.cs
@@ -1,14 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using ControlActividades.Models;
 using ControlActividades.Models.db;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.IdentityModel.Tokens;
 
@@ -16,6 +18,19 @@ namespace ControlActividades.Recursos
 {
     public class FuncionalidadesGenerales
     {
+        private ApplicationDbContext _db;
+
+        public ApplicationDbContext Db
+        {
+            get
+            {
+                return _db ?? (_db = new ApplicationDbContext());
+            }
+            private set
+            {
+                _db = value;
+            }
+        }
         public string GenerarCodigoAleatorio()
         {
             int length = 5;
@@ -25,6 +40,7 @@ namespace ControlActividades.Recursos
                 .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
+        #region JWT
         public string GenerarJwt(int idUsuario, IdentityUser emailEncontrado, string rolUsuario)
         {
             var handler = new JwtSecurityTokenHandler();
@@ -60,6 +76,25 @@ namespace ControlActividades.Recursos
 
             return claims;
         }
-        
+
+        #endregion
+
+        #region Usuario
+        public int ObtenerUsuarioId(IPrincipal User)
+        {
+            int usuarioId = 0;
+            string userId = User.Identity.GetUserId();
+
+            if (User.IsInRole(Roles.DOCENTE))
+            {
+                usuarioId = Db.tbDocentes.Where(a => a.UserId == userId).Select(a => a.DocenteId).FirstOrDefault();
+            }
+            else if (User.IsInRole(Roles.ALUMNO))
+            {
+                usuarioId = Db.tbAlumnos.Where(a => a.UserId == userId).Select(a => a.AlumnoId).FirstOrDefault();
+            }
+            return usuarioId;
+        }
+        #endregion
     }
 }
