26032b6f033d885b73e31b7c98f78177479e31c3
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-01-09 15:15:31 -0600

    Pantalla de carga
    Se quitó la pantalla de carga en cada petición fetch/ajax.
    Solo aparece en cambio de página

--- Scripts/Componentes/PantallaCarga.js
+++ Scripts/Componentes/PantallaCarga.js
@@ -1,40 +1,14 @@
-﻿    function mostrarLoader() {
-        $("#loader").addClass("visible");
-    }
+﻿function mostrarLoader() {
+    $("#loader").addClass("visible");
+}
 
 function ocultarLoader() {
     setTimeout(() => {
         $("#loader").removeClass("visible");
-    }, 10); // retrasa x milisegundos para ver la animación
+    });
 }
 
-
-    // Mostrar loader automáticamente durante cualquier petición AJAX
-    $(document).ajaxStart(function () {
-        mostrarLoader();
-    });
-
-    $(document).ajaxStop(function () {
-        ocultarLoader();
-    });
-
-    // Mostrar loader en cambio de página
-    window.addEventListener('beforeunload', function () {
-        mostrarLoader();
-    });
-
-    //Carga para fetch
-    if (window.fetch) {
-        const originalFetch = window.fetch;
-        window.fetch = async function (...args) {
-            try {
-                mostrarLoader();
-                const response = await originalFetch.apply(this, args);
-                return response;
-            } catch (error) {
-                throw error;
-            } finally {
-                ocultarLoader();
-            }
-        };
-    }
+// Mostrar loader en cambio de página
+window.addEventListener('beforeunload', function () {
+    mostrarLoader();
+});
