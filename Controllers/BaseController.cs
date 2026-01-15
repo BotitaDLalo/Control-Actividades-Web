f76332a61fd3f5867802818c04909d8bcb386aae
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-01-15 14:29:31 -0600

    Filtros de caché
    Se creó un filtro para evitar que las páginas se guarden en caché al estar impersonando y evitar mezclas de layouts.

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using ControlActividades.Filters;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlActividades.Controllers
+{
+    [Authorize]
+    [NoCache]
+    public abstract class BaseController : Controller
+    {
+    }
+}
