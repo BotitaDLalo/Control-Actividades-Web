17bd9d10998281b22e1d593dad9e52c157e519a9
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-01-18 17:37:51 -0600

    Verificacion de recibimiento de datos
    Endpoint creado para verficar que se puedan recibir grandes cantidades de datos (sin registrarlos a la bdd aún).
    Verificado.
    20k datos recibidos.

---
+++ Controllers/MigracionUsuariosController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ControlActividades.Dtos.Migracion;
+
+namespace ControlActividades.Controllers
+{
+    [RoutePrefix("api/migracion")]
+    public class MigracionUsuariosController : ApiController
+    {
+        [HttpGet]
+        [Route("ping")]
+        public IHttpActionResult Ping()
+        {
+            return Ok("Api funcionando");
+        }
+
+        [HttpPost]
+        [Route("usuarios")]
+        public IHttpActionResult MigrarUsuarios(MigracionUsuariosDto dto)
+        {
+            if(dto == null || dto.Usuarios == null || !dto.Usuarios.Any())
+            {
+                return BadRequest("El objeto DTO no puede ser nulo.");
+            }
+
+            return Ok(new
+            {
+                TotalRecibidos = dto.Usuarios.Count
+            });
+        }
+    }
+}
---
+++ Dtos/Migracion/UsuarioMigracionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlActividades.Dtos.Migracion
+{
+    public class UsuarioMigracionDto
+    {
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Correo { get; set; }
+        public string PasswordPlano { get; set; }
+        public string Rol { get; set; }
+    }
+}
