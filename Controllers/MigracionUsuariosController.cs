8bc8b06774aa3adf03ffa87c64dd78160d40e8a8
ChristianBZ360 <20680051@cuautla.tecnm.mx>
2026-01-20 00:35:54 -0600

    MIgración de usuarios
    Prueba 1. Creación del servicio y controller encargados de la migración.
    - Se pudieron insertar 6 registros a través del endpoint correctamente, respetando sus roles, y se asignaron a la tabla tbAlumnos o tbDocentes de forma exitosa

--- Controllers/MigracionUsuariosController.cs
+++ Controllers/MigracionUsuariosController.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using ControlActividades.Dtos.Migracion;
+using ControlActividades.Models;
+using ControlActividades.Services;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
-using ControlActividades.Dtos.Migracion;
 
 namespace ControlActividades.Controllers
 {
     [RoutePrefix("api/migracion")]
     public class MigracionUsuariosController : ApiController
     {
+
+        private readonly IMigracionUsuariosService _migracionService;
+
+        public MigracionUsuariosController()
+        {
+            var context = HttpContext.Current.GetOwinContext();
+            var userManager = context.GetUserManager<ApplicationUserManager>();
+            var dbContext = context.Get<ApplicationDbContext>();
+
+            _migracionService = new MigracionUsuariosService(userManager, dbContext);
+        }
+
         [HttpGet]
         [Route("ping")]
         public IHttpActionResult Ping()
@@ -18,17 +35,16 @@ namespace ControlActividades.Controllers
 
         [HttpPost]
         [Route("usuarios")]
-        public IHttpActionResult MigrarUsuarios(MigracionUsuariosDto dto)
+        public async Task<IHttpActionResult> MigrarUsuarios(MigracionUsuariosDto dto)
         {
-            if(dto == null || dto.Usuarios == null || !dto.Usuarios.Any())
+            if(dto?.Usuarios == null || !dto.Usuarios.Any())
             {
-                return BadRequest("El objeto DTO no puede ser nulo.");
+                return BadRequest("No hay usuarios.");
             }
 
-            return Ok(new
-            {
-                TotalRecibidos = dto.Usuarios.Count
-            });
+            var resultado = await _migracionService.MigrarUsuariosAsync(dto.Usuarios);
+
+            return Ok(resultado);
         }
     }
 }
---
+++ Services/MigracionResultado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlActividades.Services
+{
+    public class MigracionResultado
+    {
+        public int TotalRecibidos { get; set; }
+        public int Insertados { get; set; }
+        public int Fallidos { get; set; }
+        public List<string> Errores { get; set; } = new List<string>();
+    }
+}
